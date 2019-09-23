24d32fd0a19b22f33fe06bd5e7f3c22f917d9815
Bogdan A. Stanciu <stanciub@uni.coventry.ac.uk>
2019-09-23 20:34:32 +0100

    Normals, Culling, Lighting & Object Files

--- olc3DEngine.cpp
+++ olc3DEngine.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
+#include <math.h>
 #include "olcConsoleGameEngine.h"
+#include <fstream>
+#include <strstream>
+#include <algorithm>
 
 using namespace std;
 
+
 struct vec3d { float x, y, z; };
-struct triangle { vec3d p[3]; };
-struct mesh { vector<triangle> tris; };
+struct triangle { 
+	vec3d p[3];
+	wchar_t sym;
+	short col;
+};
+struct mesh { 
+	vector<triangle> tris;
+
+	bool LoadFromObjectFile(string sFilename) {
+
+		ifstream f(sFilename);
+		if (!f.is_open())
+			return false;
+
+		//Local cache of verts
+		vector<vec3d> verts;
+		while (!f.eof()) {
+			char line[128];
+			f.getline(line, 128);
+
+			strstream s;
+			s << line;
+
+			char junk;
+
+			if (line[0] == 'v') {
+				vec3d v;
+				s >> junk >> v.x >> v.y >> v.z;
+				verts.push_back(v);
+			}
+
+			if (line[0] == 'f') {
+				int f[3];
+				s >> junk >> f[0] >> f[1] >> f[2];
+				tris.push_back({ verts[f[0] - 1], verts[f[1] - 1], verts[f[2] - 1] });
+			}
+		}
+
+		return true;
+	}
+
+};
 struct mat4x4 { float m[4][4] = { 0 }; };
 
 class olc3dEngine : public olcConsoleGameEngine {
@@ -13,7 +58,9 @@ public:
 	olc3dEngine() { m_sAppName = L"3D DEMO"; }
 	bool OnUserCreate() override 
 	{	
-		meshCube.tris = {
+
+		meshCube.LoadFromObjectFile("VideoShip.obj");
+		/*meshCube.tris = {
 			// SOUTH
 			{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
 			{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
@@ -37,7 +84,7 @@ public:
 			// BOTTOM                                                    
 			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
 			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f }	
-		};
+		};*/
 
 		// Projection Matrix
 		float fNear = 0.1f;
@@ -79,6 +126,9 @@ public:
 		matRotX.m[2][2] = cosf(fTheta * 0.5f);
 		matRotX.m[3][3] = 1;
 
+
+		vector<triangle> vecTrianglesToRaster;
+
 		//draw triangles
 
 		for (auto tri : meshCube.tris) {
@@ -96,38 +146,99 @@ public:
 
 			// Offset into the screen
 			triTranslated = triRotatedZX;
-			triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
-			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
-			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+			triTranslated.p[0].z = triRotatedZX.p[0].z + 20.0f;
+			triTranslated.p[1].z = triRotatedZX.p[1].z + 20.0f;
+			triTranslated.p[2].z = triRotatedZX.p[2].z + 20.0f;
 
-			// Project triangles from 3D --> 2D
-			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
-			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
-			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+			vec3d normal, line1, line2;
+			line1.x = triTranslated.p[1].x - triTranslated.p[0].x;
+			line1.y = triTranslated.p[1].y - triTranslated.p[0].y;
+			line1.z = triTranslated.p[1].z - triTranslated.p[0].z;
 
-			// Scale into view
-			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
-			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
-			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
-			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
-			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
-			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
-			triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+			line2.x = triTranslated.p[2].x - triTranslated.p[0].x;
+			line2.y = triTranslated.p[2].y - triTranslated.p[0].y;
+			line2.z = triTranslated.p[2].z - triTranslated.p[0].z;
 
-			// Rasterize triangle
-			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+			normal.x = line1.y * line2.z - line1.z * line2.y;
+			normal.y = line1.z * line2.x - line1.x * line2.z;
+			normal.z = line1.x * line2.y - line1.y * line2.x;
+
+			//normalize the "normal" line with pitagora's theorem
+			float l = sqrt(pow(normal.x, 2) + pow(normal.y, 2) + pow(normal.z, 2));
+			//divise each component by l's length
+			normal.x /= l; normal.y /= l; normal.z /= l;
+
+			if (normal.x * (triTranslated.p[0].x - vCamera.x ) +
+				normal.y * (triTranslated.p[0].y - vCamera.y ) +
+				normal.z * (triTranslated.p[0].z - vCamera.z ) < 0){
+
+				//Illumination
+				vec3d light_direction = { 0.0f, 0.0f, -1.0f };
+
+				//normalize the "light direction" with pitagora's theorem
+				float l = sqrt(pow(light_direction.x, 2) + pow(light_direction.y, 2) + pow(light_direction.z, 2));
+				//divise each component by l's length
+				light_direction.x /= l; light_direction.y /= l; light_direction.z /= l;
+
+				//dot product
+				float dp = normal.x * light_direction.x + normal.y * light_direction.y + normal.z * light_direction.z;
+
+				CHAR_INFO c = GetColour(dp);
+				triTranslated.col = c.Attributes;
+				triTranslated.sym = c.Char.UnicodeChar;
+
+				// Project triangles from 3D --> 2D
+				MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+				MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+				MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+				triProjected.col = triTranslated.col;
+				triProjected.sym = triTranslated.sym;
+
+				// Scale into view
+				triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+				triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+				triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
+				triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+				triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
+				triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+				triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
+				triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+				triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+
+				//Store triangle
+				vecTrianglesToRaster.push_back(triProjected);
+			}
+		}
+		//Sort Triangles from back to front
+		sort(vecTrianglesToRaster.begin(), vecTrianglesToRaster.end(), [](triangle &t1, triangle &t2) {
+			float z1 = (t1.p[0].z + t1.p[1].z + t1.p[2].z) / 3.0f;
+			float z2 = (t2.p[0].z + t2.p[1].z + t2.p[2].z) / 3.0f;
+
+			return z1 > z2;
+		});
+
+		for(auto &triProjected: vecTrianglesToRaster){
+
+			FillTriangle(triProjected.p[0].x, triProjected.p[0].y,
 				triProjected.p[1].x, triProjected.p[1].y,
 				triProjected.p[2].x, triProjected.p[2].y,
-				PIXEL_SOLID, FG_WHITE);
+				triProjected.sym, triProjected.col);
+
+			/*DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+				triProjected.p[1].x, triProjected.p[1].y,
+				triProjected.p[2].x, triProjected.p[2].y,
+				PIXEL_SOLID, FG_WHITE);*/
 		}
 		return true;
 	}
 private:
 	mesh meshCube;
 	mat4x4 matProj;
+	vec3d vCamera;
+
 	float fTheta;
+
+
 	void MultiplyMatrixVector(vec3d &i, vec3d &o, mat4x4 &m)
 	{
 		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
@@ -140,11 +251,45 @@ private:
 			o.x /= w; o.y /= w; o.z /= w;
 		}
 	}
+
+	// Taken From Command Line Webcam Video
+	CHAR_INFO GetColour(float lum)
+	{
+		short bg_col, fg_col;
+		wchar_t sym;
+		int pixel_bw = (int)(13.0f*lum);
+		switch (pixel_bw)
+		{
+		case 0: bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID; break;
+
+		case 1: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_QUARTER; break;
+		case 2: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_HALF; break;
+		case 3: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_THREEQUARTERS; break;
+		case 4: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_SOLID; break;
+
+		case 5: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_QUARTER; break;
+		case 6: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_HALF; break;
+		case 7: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_THREEQUARTERS; break;
+		case 8: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_SOLID; break;
+
+		case 9:  bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_QUARTER; break;
+		case 10: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_HALF; break;
+		case 11: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_THREEQUARTERS; break;
+		case 12: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_SOLID; break;
+		default:
+			bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID;
+		}
+
+		CHAR_INFO c;
+		c.Attributes = bg_col | fg_col;
+		c.Char.UnicodeChar = sym;
+		return c;
+	}
 };
 
 int main() {
 	olc3dEngine demo;
-	if (demo.ConstructConsole(100, 140, 4, 4))
+	if (demo.ConstructConsole(120, 180, 4, 4))
 		demo.Start();
 	return 0;
 }
